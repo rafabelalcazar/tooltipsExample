7a80ebdd239ec38731580bde1a3dc28faed9ceea
rafabelalcazar <rafaelb@unicauca.edu.co>
2019-09-10 16:16:41 -0500

    Para solucionar los problemas de los movimientos en el overlay se debe quitar la barra de estato con StatusBar o darle un valor absoluto al height del view

--- components/Tooltip.js
+++ components/Tooltip.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native'
+import { Text, StyleSheet, View, Button, TouchableOpacity, StatusBar } from 'react-native'
 import PopoverTooltip from 'react-native-popover-tooltip';
 
 export default class Tooltip extends Component {
@@ -9,6 +9,30 @@ export default class Tooltip extends Component {
     render() {
         return (
             <View style={{ flex: 1, alignSelf: 'stretch', alignItems: 'center', justifyContent: 'flex-start', backgroundColor: '#fff' }}>
+                <StatusBar hidden={true} />
+
+
+                <Text>Default Effect</Text>
+                <PopoverTooltip
+                    ref='tooltip1'
+                    buttonComponent={
+                        <View style={{ width: 200, height: 50, backgroundColor: 'orange', justifyContent: 'center', alignItems: 'center', borderRadius: 5 }}>
+                            <Text>Press Me</Text>
+                        </View>
+                    }
+                    items={[
+                        {
+                            label: 'Item 1',
+                            onPress: () => { }
+                        },
+                        {
+                            label: 'Item 2',
+                            onPress: () => { }
+                        }
+                    ]}
+                // animationType='timing'
+                // using the default timing animation
+                />
 
                 <Text>Spring Effect</Text>
                 <PopoverTooltip
@@ -33,30 +57,6 @@ export default class Tooltip extends Component {
                     animationType='spring' // spring-damper animation
                     springConfig={{ tension: 100, friction: 3 }} // tension controls the potential of the spring effect,
                 // friction controls the damper effect
-                />
-
-<Text>Default Effect</Text>
-                <PopoverTooltip
-                    ref='tooltip1'
-                    buttonComponent={
-                        <View style={{ width: 200, height: 50, backgroundColor: 'orange', justifyContent: 'center', alignItems: 'center', borderRadius: 5 }}>
-                            <Text>
-                                Press Me
-              </Text>
-                        </View>
-                    }
-                    items={[
-                        {
-                            label: 'Item 1',
-                            onPress: () => { }
-                        },
-                        {
-                            label: 'Item 2',
-                            onPress: () => { }
-                        }
-                    ]}
-                // animationType='timing'
-                // using the default timing animation
                 />
 
                 <Text>Button Expansion</Text>
@@ -137,16 +137,16 @@ export default class Tooltip extends Component {
                     opacityChangeDuration={1000} />
 
                 <View style={{ height: 40 }} />
-                {/* <TouchableOpacity
+                <TouchableOpacity
                     style={{ width: 200, height: 50, backgroundColor: '#B0A4E3', justifyContent: 'center', alignItems: 'center', borderRadius: 5 }}
                     onPress={() => {
-                        this.refs['tooltip' + this.state.order].toggle();  open popover tooltips one by one
+                        this.refs['tooltip' + this.state.order].toggle();//  open popover tooltips one by one
                         this.setState({ order: (this.state.order) % 5 + 1 });
                     }}>
                     <Text>
                         Open Tooltip {this.state.order}
                     </Text>
-                </TouchableOpacity> */}
+                </TouchableOpacity>
             </View>
 
         )
